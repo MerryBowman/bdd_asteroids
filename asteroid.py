c1915c6c447d66a0924cbe6747ed98aa48d5ec85
MerryBowman <trenzvorfre@hotmail.com>
2025-07-14 20:41:59 -0500

    course final

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import *
 
@@ -11,3 +12,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+            split_radius = self.radius - ASTEROID_MIN_RADIUS
+            
+            asteroid = Asteroid(self.position.x, self.position.y, split_radius)
+            asteroid.velocity = pygame.math.Vector2.rotate(self.velocity, random_angle) * 1.2
+
+            asteroid = Asteroid(self.position.x, self.position.y, split_radius)
+            asteroid.velocity = pygame.math.Vector2.rotate(self.velocity, (0 - random_angle)) * 1.2
+
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame
 import sys
 from constants import *
-from player import Player
+from player import *
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 
-
+#player_score = 0
 
 def main():
     pygame.init()
@@ -15,10 +15,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     field = AsteroidField()
@@ -33,8 +35,15 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.check_collision(shot) == True:
+                    shot.kill()
+                    asteroid.split()
+                    #player_score += 100
+                
             if asteroid.check_collision(player) == True:
                 print("Game Over!")
+                #print(f"Scored {player_score} points!")
                 sys.exit()
 
         screen.fill("black")
